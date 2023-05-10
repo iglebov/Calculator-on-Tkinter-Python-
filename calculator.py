cec4c119cc89f312b4c020956d6d557562d31b88
iglebov <iglebov40@gmail.com>
2023-05-10 14:21:19 +0300

    style: Change comments of functions and variables

--- calculator.py
+++ calculator.py
@@ -1,33 +1,27 @@
 # Importing module tkinter to create graphic interface
 from tkinter import *
 
-# Basic data
+# Starting parameters for UI
 root = Tk()
 root.title("Calculator")
 root.geometry("350x380+400+450")
-
 equation = StringVar()
 expression = ""
-
-expression_field = Entry(textvariable=equation, \
-            font="Arial 32", justify=RIGHT)
-expression_field.place(relx=.5, rely=.2, \
-            height = 140, width = 350, anchor="c")
-
-# Creating variable for (dot) control
+# Creating variable for dot appearance control
 dot = 0
 
-# Creating functions for buttons
+
 def click_button(num):
+    """Sets expression after activity"""
     global expression, dot
-    
+
     if (num == ".") and (dot == 0):
         dot = 1
         expression = expression + str(num)
         equation.set(expression)
     elif (num == ".") and (dot == 1):
         pass
-    elif num in '+-/*':
+    elif num in "+-/*":
         dot = 0
         expression = expression + str(num)
         equation.set(expression)
@@ -35,171 +29,204 @@ def click_button(num):
         expression = expression + str(num)
         equation.set(expression)
 
-# Function to find expression
+
 def equal():
+    """Finds result of expression"""
     try:
         global expression
         total = str(eval(expression))
         equation.set(total)
         expression = total
-    except:
+    except:  # noqa
         equation.set("Error")
         expression = ""
 
-# Function to clear expression
+
 def clear():
+    """Clears expression."""
     global expression
     expression = ""
     equation.set("")
 
+
 def delete():
+    """Deletes character."""
     global expression
     expression = expression[:-1]
     equation.set(expression)
 
-# Button '9'
-btn_9 = Button(text="9",
-            background="#808080",
-            foreground="#FFFAF0",
-            font="Arial 18",
-            command=lambda: click_button("9"))
+
+expression_field = Entry(textvariable=equation, font="Arial 32", justify=RIGHT)
+expression_field.place(relx=0.5, rely=0.2, height=140, width=350, anchor="center")
+
+btn_9 = Button(
+    text="9",
+    background="#808080",
+    foreground="#FFFAF0",
+    font="Arial 18",
+    command=lambda: click_button("9"),
+)
 btn_9.place(x=140, y=100, height=70, width=70, bordermode=OUTSIDE)
 
-# Button '8'
-btn_8 = Button(text="8",
-            background="#808080",
-            foreground="#FFFAF0",
-            font="Arial 18",
-            command=lambda: click_button("8"))
+btn_8 = Button(
+    text="8",
+    background="#808080",
+    foreground="#FFFAF0",
+    font="Arial 18",
+    command=lambda: click_button("8"),
+)
 btn_8.place(x=70, y=100, height=70, width=70, bordermode=OUTSIDE)
 
-# Button '7'
-btn_7 = Button(text="7",
-            background="#808080",
-            foreground="#FFFAF0",
-            font="Arial 18",
-            command=lambda: click_button("7"))
+btn_7 = Button(
+    text="7",
+    background="#808080",
+    foreground="#FFFAF0",
+    font="Arial 18",
+    command=lambda: click_button("7"),
+)
 btn_7.place(x=0, y=100, height=70, width=70, bordermode=OUTSIDE)
 
-# Button '6'
-btn_6 = Button(text="6",
-            background="#808080",
-            foreground="#FFFAF0",
-            font="Arial 18",
-            command=lambda: click_button("6"))
+btn_6 = Button(
+    text="6",
+    background="#808080",
+    foreground="#FFFAF0",
+    font="Arial 18",
+    command=lambda: click_button("6"),
+)
 btn_6.place(x=140, y=170, height=70, width=70, bordermode=OUTSIDE)
 
-# Button '5'
-btn_5 = Button(text="5",
-            background="#808080",
-            foreground="#FFFAF0",
-            font="Arial 18",
-            command=lambda: click_button("5"))
+
+btn_5 = Button(
+    text="5",
+    background="#808080",
+    foreground="#FFFAF0",
+    font="Arial 18",
+    command=lambda: click_button("5"),
+)
 btn_5.place(x=70, y=170, height=70, width=70, bordermode=OUTSIDE)
 
-# Button '4'
-btn_4 = Button(text="4",
-            background="#808080",
-            foreground="#FFFAF0",
-            font="Arial 18",
-            command=lambda: click_button("4"))
+
+btn_4 = Button(
+    text="4",
+    background="#808080",
+    foreground="#FFFAF0",
+    font="Arial 18",
+    command=lambda: click_button("4"),
+)
 btn_4.place(x=0, y=170, height=70, width=70, bordermode=OUTSIDE)
 
-# Button '3'
-btn_3 = Button(text="3",
-            background="#808080",
-            foreground="#FFFAF0",
-            font="Arial 18",
-            command=lambda: click_button("3"))
+
+btn_3 = Button(
+    text="3",
+    background="#808080",
+    foreground="#FFFAF0",
+    font="Arial 18",
+    command=lambda: click_button("3"),
+)
 btn_3.place(x=140, y=240, height=70, width=70, bordermode=OUTSIDE)
 
-# Button '2'
-btn_2 = Button(text="2",
-            background="#808080",
-            foreground="#FFFAF0",
-            font="Arial 18",
-            command=lambda: click_button("2"))
+
+btn_2 = Button(
+    text="2",
+    background="#808080",
+    foreground="#FFFAF0",
+    font="Arial 18",
+    command=lambda: click_button("2"),
+)
 btn_2.place(x=70, y=240, height=70, width=70, bordermode=OUTSIDE)
 
-# Button '1'
-btn_1 = Button(text="1",
-            background="#808080",
-            foreground="#FFFAF0",
-            font="Arial 18",
-            command=lambda: click_button("1"))
+
+btn_1 = Button(
+    text="1",
+    background="#808080",
+    foreground="#FFFAF0",
+    font="Arial 18",
+    command=lambda: click_button("1"),
+)
 btn_1.place(x=0, y=240, height=70, width=70, bordermode=OUTSIDE)
 
-# Button '0'
-btn_0 = Button(text="0",
-            background="#808080",
-            foreground="#FFFAF0",
-            font="Arial 18",
-            command=lambda: click_button("0"))
+
+btn_0 = Button(
+    text="0",
+    background="#808080",
+    foreground="#FFFAF0",
+    font="Arial 18",
+    command=lambda: click_button("0"),
+)
 btn_0.place(x=0, y=310, height=70, width=70, bordermode=OUTSIDE)
 
-# Button '+'
-btn_plus = Button(text="+",
-            background="#808080",
-            foreground="#FFFAF0",
-            font="Arial 18",
-            command=lambda: click_button("+"))
+
+btn_plus = Button(
+    text="+",
+    background="#808080",
+    foreground="#FFFAF0",
+    font="Arial 18",
+    command=lambda: click_button("+"),
+)
 btn_plus.place(x=210, y=170, height=70, width=70, bordermode=OUTSIDE)
 
-# Button '-'
-btn_minus = Button(text="-",
-                    background="#808080",
-                    foreground="#FFFAF0",
-                    font="Arial 18",
-                    command=lambda: click_button("-"))
+
+btn_minus = Button(
+    text="-",
+    background="#808080",
+    foreground="#FFFAF0",
+    font="Arial 18",
+    command=lambda: click_button("-"),
+)
 btn_minus.place(x=280, y=170, height=70, width=70, bordermode=OUTSIDE)
 
-# Button '*'
-btn_multiply = Button(text="*",
-                    background="#808080",
-                    foreground="#FFFAF0",
-                    font="Arial 18",
-                    command=lambda: click_button("*"))
+btn_multiply = Button(
+    text="*",
+    background="#808080",
+    foreground="#FFFAF0",
+    font="Arial 18",
+    command=lambda: click_button("*"),
+)
 btn_multiply.place(x=210, y=240, height=70, width=70, bordermode=OUTSIDE)
 
-# Button '/'
-btn_divide = Button(text="/",
-                    background="#808080",
-                    foreground="#FFFAF0",
-                    font="Arial 18",
-                    command=lambda: click_button("/"))
+btn_divide = Button(
+    text="/",
+    background="#808080",
+    foreground="#FFFAF0",
+    font="Arial 18",
+    command=lambda: click_button("/"),
+)
 btn_divide.place(x=280, y=240, height=70, width=70, bordermode=OUTSIDE)
 
-# Button '='
-btn_equal = Button(text="=",
-            background="#808080",
-            foreground="#FFFAF0",
-            font="Arial 18",
-            command=equal)
+
+btn_equal = Button(
+    text="=", background="#808080", foreground="#FFFAF0", font="Arial 18", command=equal
+)
 btn_equal.place(x=210, y=310, height=70, width=140, bordermode=OUTSIDE)
 
-# Button '.'
-btn_dot = Button(text="{}".format("."),
-            background="#808080",
-            foreground="#FFFAF0",
-            font="Arial 18",
-            command=lambda: click_button("."))
+
+btn_dot = Button(
+    text="{}".format("."),
+    background="#808080",
+    foreground="#FFFAF0",
+    font="Arial 18",
+    command=lambda: click_button("."),
+)
 btn_dot.place(x=70, y=310, height=70, width=70, bordermode=OUTSIDE)
 
-# Button 'C' (clear)
-btn_clear = Button(text="{}".format("C"),
-            background="#A52A2A",
-            foreground="#FFFAF0",
-            font="Arial 18",
-            command=clear)
+
+btn_clear = Button(
+    text="{}".format("C"),
+    background="#A52A2A",
+    foreground="#FFFAF0",
+    font="Arial 18",
+    command=clear,
+)
 btn_clear.place(x=140, y=310, height=70, width=70, bordermode=OUTSIDE)
 
-# Button 'delete'
-btn_delete = Button(text="{}".format("<="),
-            background="#A5AA5B",
-            foreground="#FFFAF0",
-            font="Arial 18",
-            command=delete)
+btn_delete = Button(
+    text="{}".format("<="),
+    background="#A5AA5B",
+    foreground="#FFFAF0",
+    font="Arial 18",
+    command=delete,
+)
 btn_delete.place(x=210, y=100, height=70, width=140, bordermode=OUTSIDE)
 
-# Starting up graphic interface
-root.mainloop()
+if __name__ == "__main__":
+    root.mainloop()
